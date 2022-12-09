d2018459184018b5423d8328798fe442d751ae24
Mike Treit <mtreit@ntdev.microsoft.com>
2022-12-09 14:17:47 -0800

    Day 9 part 1.

--- csharp/AdventOfCode2022/AoC.Puzzles.2022/Puzzles/Day08.cs
+++ csharp/AdventOfCode2022/AoC.Puzzles.2022/Puzzles/Day08.cs
@@ -1,18 +1,218 @@
 ﻿using AoC.Common.Attributes;
 using AoC.Common.Interfaces;
-using System.Text;
+using System.Runtime.Versioning;
 
 namespace AoC.Puzzles._2022.Puzzles;
 
-[Puzzle(2022, 8, "Treetop Tree House")]
-public class Day08 : IPuzzle<string[]>
+struct Coords
 {
-    // Coordinates and height
-    Dictionary<(int, int), int> _visibleTrees;
+    public int X;
+    public int Y;
+}
 
-    public Day08()
+class Rope
+{
+    HashSet<Coords> _tailVisited = new HashSet<Coords>();
+
+    public Coords Head { get; set; } = new Coords { X = 20, Y = 30 };
+    public Coords Tail { get; set; } = new Coords { X = 20, Y = 30 };
+
+    public Rope()
     {
-        _visibleTrees = new Dictionary<(int, int), int>();
+        _tailVisited.Add(Tail);
+    }
+
+    public void Move(char direction, int amount)
+    {
+        switch (direction)
+        {
+            case 'R':
+                for (int i = 0; i < amount; i++)
+                {
+                    MoveRight();
+                }
+                break;
+            case 'L':
+                for (int i = 0; i < amount; i++)
+                {
+                    MoveLeft();
+                }
+                break;
+            case 'U':
+                for (int i = 0; i < amount; i++)
+                {
+                    MoveUp();
+                }
+                break;
+            case 'D':
+                for (int i = 0; i < amount; i++)
+                {
+                    MoveDown();
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    public int GetTailVisitedCount()
+    {
+        return _tailVisited.Count;
+    }
+
+    void MoveRight()
+    {
+        var head = Head;
+        Clear();
+        head.Y++;
+        Head = head;
+        Draw();
+        Clear();
+        UpdateTail();
+        Draw();
+    }
+
+    void MoveLeft()
+    {
+        Clear();
+        var head = Head;
+        head.Y--;
+        Head = head;
+        Draw();
+        Clear();
+        UpdateTail();
+        Draw();
+    }
+
+    void MoveUp()
+    {
+        Clear();
+        var head = Head;
+        head.X--;
+        Head = head;
+        Draw();
+        Clear();
+        UpdateTail();
+        Draw();
+    }
+
+    void MoveDown()
+    {
+        var head = Head;
+        Clear();
+        head.X++;
+        Head = head;
+        Draw();
+        Clear();
+        UpdateTail();
+        Draw();
+    }
+
+    void UpdateTail()
+    {
+        int ydiff = Head.Y - Tail.Y;
+        int xdiff = Head.X - Tail.X;
+
+        var tail = Tail;
+
+        if ((ydiff == 1 && xdiff == -2) || (ydiff == 2 && xdiff == -1))
+        {
+            // Diagonal up right
+            tail.Y++;
+            tail.X--;
+        }
+        else if ((ydiff == 1 && xdiff == 2) || (ydiff == 2 && xdiff == 1))
+        {
+            // Diagonal down right.
+            tail.Y++;
+            tail.X++;
+        }
+        else if ((ydiff == -1 && xdiff == -2) || (ydiff == -2 && xdiff == -1))
+        {
+            // Diagnoal up left
+            tail.Y--;
+            tail.X--;
+        }
+        else if ((ydiff == -1 && xdiff == 2) || (ydiff == -2 && xdiff == 1))
+        {
+            // Diagnonal down left
+            tail.Y--;
+            tail.X++;
+        }
+        else if (ydiff == 2 )
+        {
+            // Move right
+            tail.Y++;
+        }
+        else if (ydiff == -2)
+        {
+            // Move left
+            tail.Y--;
+        }
+        else if (xdiff == 2)
+        {
+            // Move down
+            tail.X++;
+        }
+        else if (xdiff == -2)
+        {
+            // Move up
+            tail.X--;
+        }
+
+        Tail = tail;
+
+        _tailVisited.Add(tail);
+    }
+
+    public void Draw()
+    {
+        if (Environment.GetEnvironmentVariable("DRAW") == null)
+        {
+            return;
+        }
+
+        foreach (var coord in _tailVisited)
+        {
+            Console.SetCursorPosition(coord.Y, coord.X);
+            Console.Write("#");
+        }
+
+        Console.SetCursorPosition(Tail.Y, Tail.X);
+        Console.Write("T");
+
+        Console.SetCursorPosition(Head.Y, Head.X);
+        Console.Write("H");
+
+        Thread.Sleep(50);
+    }
+
+    public void Clear()
+    {
+        if (Environment.GetEnvironmentVariable("DRAW") == null)
+        {
+            return;
+        }
+
+        foreach (var coord in _tailVisited)
+        {
+            Console.SetCursorPosition(coord.Y, coord.X);
+            Console.Write(" ");
+        }
+
+        Console.SetCursorPosition(Head.Y, Head.X);
+        Console.Write(" ");
+
+        Console.SetCursorPosition(Tail.Y, Tail.X);
+        Console.Write(" ");
+    }
+}
+
+[Puzzle(2022, 9, "Rope Bridge")]
+public class Day09 : IPuzzle<string[]>
+{
+    public Day09()
+    {
     }
 
     public string[] Parse(string inputText)
@@ -20,230 +220,36 @@ public class Day08 : IPuzzle<string[]>
         return inputText.Split(Environment.NewLine).Where(x => x.Length > 0).ToArray();
     }
 
+    [SupportedOSPlatform("windows")]
     public string Part1(string[] input)
     {
-        CountVisibleRight(input);
-        CountVisibleLeft(input);
-        CountVisibleDown(input);
-        CountVisibleUp(input);
+        if (Environment.GetEnvironmentVariable("DRAW") == null)
+        {
+            Console.SetBufferSize(10000, 10000);
+            Console.Clear();
+        }
 
-        return _visibleTrees.Count.ToString();
+        var rope = new Rope();
+        rope.Draw();
+
+        foreach (var str in input)
+        {
+            (char dir, int count) = Decode(str);
+            rope.Move(dir, count);
+        }
+
+        return rope.GetTailVisitedCount().ToString();
     }
 
     public string Part2(string[] input)
     {
-        int maxScenicScore = 0;
-
-        for (int i = 0; i < input.Length; i++)
-        {
-            for (int j = 0; j < input[i].Length; j++)
-            {
-                var scenicScore = GetScenicScore(input, (i, j));
-
-                maxScenicScore = Math.Max(maxScenicScore, scenicScore);
-            }
-        }
-
-        return maxScenicScore.ToString();
+        return "";
     }
 
-    void CountVisibleRight(string[] input)
+    static (char dir, int count) Decode(string input)
     {
-        for (int i = 0; i < input.Length; i++)
-        {
-            int maxHeight = (int)char.GetNumericValue(input[i][0]);
-
-            for (int j = 0; j < input[i].Length; j++)
-            {
-                int height = (int)char.GetNumericValue(input[i][j]);
-
-                if (j == 0)
-                {
-                    // Left edge.
-                    _visibleTrees[(i, j)] = height;
-                    continue;
-                }
-
-                if (height > maxHeight)
-                {
-                    maxHeight = height;
-                    _visibleTrees[(i, j)] = height;
-                }
-            }
-        }
+        var dir = input[0];
+        int count = int.Parse(input.Substring(2));
+        return (dir, count);
     }
-
-    void CountVisibleLeft(string[] input)
-    {
-        for (int i = 0; i < input.Length; i++)
-        {
-            int maxHeight = (int)char.GetNumericValue(input[i][input[i].Length - 1]);
-
-            for (int j = input[i].Length - 1; j >= 0; j--)
-            {
-                int height = (int)char.GetNumericValue(input[i][j]);
-
-                if (j == input[i].Length - 1)
-                {
-                    // Right edge.
-                    _visibleTrees[(i, j)] = height;
-                    continue;
-                }
-
-                if (height > maxHeight)
-                {
-                    maxHeight = height;
-                    _visibleTrees[(i, j)] = height;
-                }
-            }
-        }
-    }
-
-    void CountVisibleDown(string[] input)
-    {
-        for (int j = 0; j < input[0].Length; j++)
-        {
-            int maxHeight = (int)char.GetNumericValue(input[0][j]);
-
-            for (int i = 0; i < input.Length; i++)
-            {
-                int height = (int)char.GetNumericValue(input[i][j]);
-
-                if (i == 0)
-                {
-                    // Top edge.
-                    _visibleTrees[(i, j)] = height;
-                    continue;
-                }
-
-                if (height > maxHeight)
-                {
-                    maxHeight = height;
-                    _visibleTrees[(i, j)] = height;
-                }
-            }
-        }
-    }
-
-    void CountVisibleUp(string[] input)
-    {
-        for (int j = 0; j < input[0].Length; j++)
-        {
-            int maxHeight = (int)char.GetNumericValue(input[input.Length - 1][j]);
-
-            for (int i = input.Length - 1; i >= 0; i--)
-            {
-                int height = (int)char.GetNumericValue(input[i][j]);
-
-                if (i == input.Length - 1)
-                {
-                    // Bottom edge.
-                    _visibleTrees[(i, j)] = height;
-                    continue;
-                }
-
-                if (height > maxHeight)
-                {
-                    maxHeight = height;
-                    _visibleTrees[(i, j)] = height;
-                }
-            }
-        }
-    }
-
-    int GetScenicScore(string[] input, (int X, int Y) coords)
-    {
-        int up = ScenicScoreUp(input, coords);
-        int left = ScenicScoreLeft(input, coords);
-        int right = ScenicScoreRight(input, coords);
-        int down = ScenicScoreDown(input, coords);
-
-        int score = right * left * down * up;
-
-        return score;
-    }
-
-    int ScenicScoreRight(string[] input, (int X, int Y) coords)
-    {
-        int score = 0;
-        var current = input[coords.X];
-        var height = (int)char.GetNumericValue(current[coords.Y]);
-
-        for (int j = coords.Y + 1; j < current.Length; j++)
-        {
-            var otherHeight = (int)char.GetNumericValue(current[j]);
-
-            score++;
-
-            if (otherHeight >= height)
-            {
-                break;
-            }
-        }
-
-        return score;
-    }
-
-    int ScenicScoreLeft(string[] input, (int X, int Y) coords)
-    {
-        int score = 0;
-        var current = input[coords.X];
-        var height = (int)char.GetNumericValue(current[coords.Y]);
-
-        for (int j = coords.Y - 1; j >= 0; j--)
-        {
-            var otherHeight = (int)char.GetNumericValue(current[j]);
-
-            score++;
-
-            if (otherHeight >= height)
-            {
-                break;
-            }
-
-        }
-
-        return score;
-    }
-
-    int ScenicScoreDown(string[] input, (int X, int Y) coords)
-    {
-        int score = 0;
-        var current = input[coords.X];
-        var height = (int)char.GetNumericValue(current[coords.Y]);
-        for (int i = coords.X + 1; i < input.Length; i++)
-        {
-            var otherHeight = (int)char.GetNumericValue(input[i][coords.Y]);
-
-            score++;
-
-            if (otherHeight >= height)
-            {
-                break;
-            }
-        }
-
-        return score;
-    }
-
-    int ScenicScoreUp(string[] input, (int X, int Y) coords)
-    {
-        int score = 0;
-        var current = input[coords.X];
-        var height = (int)char.GetNumericValue(current[coords.Y]);
-        for (int i = coords.X - 1; i >= 0; i--)
-        {
-            var otherHeight = (int)char.GetNumericValue(input[i][coords.Y]);
-
-            score++;
-
-            if (otherHeight >= height)
-            {
-                break;
-            }
-        }
-
-        return score;
-    }
-
 }
